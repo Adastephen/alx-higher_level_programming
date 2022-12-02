8fca01ff0c2657db815fd9ba383120643c43df37
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-12-01 16:44:33 -0800

    0x02

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from calculator_1 import calculator
+
+    a = int(a)
+    b = int(b)
+
+    a = 10
+    b = 5
+
+    add = calculator.add()
+        add.append("{} + {} = {}" format. a, b add(a, b)
+
+    sub = calculator.sub()
+        sub.append("{} - {} = {}" format. a, b sub(a, b)
+    
+    mul = calculator.mul()
+        mul.append("{} * {} = {}" format. a, b mul(a, b)
+
+    div = calculator.div()
+        div.append("{} / {} = {}" format. a. b div(a, b)
+
