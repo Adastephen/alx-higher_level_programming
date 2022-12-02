b586da85ad9507b741c3caff0b47d64bac1135a5
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-12-01 17:30:53 -0800

    0x02

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
+"""print the addition, subtraction. multiplication and quotient of 10 and 5"""
     from calculator_1 import add, sub, mul, div
     
     a = 10
