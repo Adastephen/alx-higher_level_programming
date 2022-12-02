c2f8ea257828103acd564872f66a0b508efa10e4
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-12-01 17:22:48 -0800

    0x02

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
 
 from add_0 import add
@@ -7,6 +6,4 @@ from add_0 import add
 a = 1
 b = 2
 
-a = int(a)
-b = int(b)
-print(f"{} + {} = {}" (a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
 
-    from calculator_1 import add,sub,mul,div
-
-    a = int(a)
-    b = int(b)
-
+    from calculator_1 import add, sub, mul, div
+    
     a = 10
     b = 5
 
