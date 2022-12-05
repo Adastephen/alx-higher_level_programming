436626a1e7b326c1a60438950b4cb6dd4f238d74
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2022-12-04 17:26:49 -0800

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ elif number < 0:
 
 
 if Last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, Last_digit))
+    print("Last digit of {0} is {1} and is greater than 5".format(number, Last_digit))
 elif Last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, Last_digit))
+    print("Last digit of {0} is {1} and is 0".format(number, Last_digit))
 elif Last_digit < 6 and Last_digit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, Last_digit))
+    print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, Last_digit))
