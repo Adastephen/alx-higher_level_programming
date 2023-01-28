b49d2ecf77b9e69c7e61ba2f036c7f6423f4f79d
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2023-01-28 12:59:56 -0800

    1-calculation.py

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -39,13 +39,11 @@ def mul(a, b):
 
 
 def div(a, b):
-     """My division function
-
-      Args:
-      a: first integer
-      b: second integer
-
-      Returns:
-      The return value. a / b
-      """
-     return int(a / b)
+    """My division function
+    Args:
+    a: first integer
+    b: second integer
+    Returns:
+    The return value. a / b
+    """
+    return int(a / b)
