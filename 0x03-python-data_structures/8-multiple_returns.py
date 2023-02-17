09dd74dcbeadb681b3b75cb19133bd2fa5895928
Adakole stephen owochoyale <adakolestephen09@gmail.com>
2023-02-17 04:45:44 -0800

    8-main.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,6 @@ def multiple_returns(sentence):
     new_tuple += len(sentence)
     sec_tuple = sentence[0]
     if new_tuple == 0:
-        sec_tuple = None
-        return (sec_tuple)
+        return ("None") 
     else:
         return (new_tuple, sec_tuple)
