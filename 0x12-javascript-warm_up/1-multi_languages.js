74e32173041b7a7994cf0d19f59b3eccd280a198
Your Name <you@example.com>
2023-06-16 00:53:03 +0100

    task2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// write a script that prints 3 lines.
+
+const firstLine = "C is fun";
+const secondLine = "Pyhton is cool";
+const thirdLine = "JavaScipt is amazing";
+
+console.log(firstLine); //C is fun
+console.log(secondLine); //Python is cool
+console.log(thirdLine); // JavaScript is amazing
